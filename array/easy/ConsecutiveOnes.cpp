59dc80797ebed1b059f4d1a7bef66c77bb0705b0
Kunal Chauhan <114133835+kunalchauhan6673@users.noreply.github.com>
2026-07-04 16:34:03 +0530

    better code

--- array/easy/ConsecutiveOnes.cpp
+++ array/easy/ConsecutiveOnes.cpp
@@ -1,27 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
-int maxConsecutiveOnes(int a[], int n)
-{
-    int c,maxc;
-    c=maxc=0;
-    for(int i=0; i<n; i++)
-    {
-        if(a[i])
-        {
-            c++;
+int findMaxConsecutiveOnes(int a[], int n) {
+    int count = 0;
+    int maxCount = 0;
+    for(int i = 0; i < n; i++) {
+        if(a[i] == 1) {
+            count++;
+        if(maxCount < count) {
+                maxCount = count;
+            }
+        } else {
+            count = 0;
         }
-        else
-        {
-            maxc=max(maxc,c);
-            c=0;
-        }
-    }
-    return maxc;
+    }  
+    return maxCount;
 }
 int main()
 {
     int a[]= {1, 1, 0, 0, 1, 1, 1, 0,1};
     int n=sizeof(a)/sizeof(a[0]);
-    cout<<maxConsecutiveOnes(a,n)<<" ";
+    cout<<findMaxConsecutiveOnes(a,n)<<" ";
     return 0;
 }
