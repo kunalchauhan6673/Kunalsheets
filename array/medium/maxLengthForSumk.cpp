b7283a7f2ebd34a2059e4f367b961f5abf670cf1
Kunal Chauhan <114133835+kunalchauhan6673@users.noreply.github.com>
2026-07-05 13:31:25 +0530

    brute solution

---
+++ array/medium/maxLengthForSumk.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int longestSubarray(vector<int> &nums, int k)
+{      // TC: o(n^2) and sc: o(1)
+        int n = nums.size();
+        int maxLength = 0;
+        for(int i=0;i<n;i++){
+            int sum=0,length=0;
+            for(int j=i;j<n;j++){
+                sum+=nums[j];
+                if(sum==k)
+                     maxLength = max(maxLength, j - i + 1);
+            }
+        }
+        return maxLength;
+}
+
+int main()
+{
+    vector<int> a = { 10,1,1,1,1,1,5 };
+    int k = 10;
+    int len = longestSubarray(a, k);
+
+    cout << "The length of the longest subarray is: " << len << "\n";
+    return 0;
+}
