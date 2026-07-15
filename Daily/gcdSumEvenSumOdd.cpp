42700b2170b8fc6b8ddcc91289d827c885994023
Kunal Chauhan <114133835+kunalchauhan6673@users.noreply.github.com>
2026-07-15 12:57:56 +0530

    leetcode daily, easy

---
+++ Daily/gcdSumEvenSumOdd.cpp
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/gcd-of-odd-and-even-sums/description
+class Solution {
+public:
+    int gdc(int a,int b){
+        while(b!=0){
+            int temp = a%b;
+            a=b;
+            b=temp;
+        }
+        return a;
+    }
+    int gcdOfOddEvenSums(int n) {
+        int sumEven = (1+n)*n; // calculates sum of n smallest even number
+        int sumOdd = n*n;     // calculates sum of n smallest odd number
+        return gcd(sumOdd, sumEven);
+    }
+};
