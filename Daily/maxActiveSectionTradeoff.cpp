8459190039de597fec9046bc0d0c263bbba0ff72
Kunal Chauhan <114133835+kunalchauhan6673@users.noreply.github.com>
2026-07-21 15:15:59 +0530

    added comments

--- Daily/maxActiveSectionTradeoff.cpp
+++ Daily/maxActiveSectionTradeoff.cpp
@@ -6,30 +6,62 @@
 class Solution {
 public:
     int maxActiveSectionsAfterTrade(string s) {
-     int n=s.size();
-     int maxMise=0;
-     int ones=0,prev=-1;
-     bool seenOne=false;
-     for(int i=0;i<n;)
-     {
-       int j=i;
-        while(j<n && s[i]==s[j]){
-            j++;
-        }
-        int len = j-i;
-        if(s[i] == '1'){
-            seenOne=true;
-            ones+=len;
-        }
-        else{
-            if(seenOne && prev!=-1){
-                maxMise=max(maxMise,prev+len);
+        int n = s.size();
+
+        // Stores the maximum number of inactive sections
+        // that can be converted to active after one trade.
+        int maxMise = 0;
+
+        // Total number of active ('1') sections already present.
+        int ones = 0;
+
+        // Length of the previous zero block.
+        int prev = -1;
+
+        // Indicates whether we have encountered a block of '1's.
+        bool seenOne = false;
+
+        // Traverse the string block by block.
+        for (int i = 0; i < n;) {
+
+            int j = i;
+
+            // Find the length of the current block
+            // (continuous sequence of same characters).
+            while (j < n && s[i] == s[j]) {
+                j++;
             }
-            prev=len;
-            seenOne=false;
+
+            int len = j - i;
+
+            // Current block consists of active sections.
+            if (s[i] == '1') {
+                seenOne = true;
+                ones += len;
+            }
+            // Current block consists of inactive sections.
+            else {
+
+                // Pattern found:
+                // previous zero block -> one block -> current zero block
+                // These two zero blocks can be merged after one trade.
+                if (seenOne && prev != -1) {
+                    maxMise = max(maxMise, prev + len);
+                }
+
+                // Store current zero block for future merging.
+                prev = len;
+
+                // Reset because we are currently on a zero block.
+                seenOne = false;
+            }
+
+            // Move to the next block.
+            i = j;
         }
-        i=j;
-     }
-     return ones+maxMise;
-     }
+
+        // Existing active sections + maximum inactive sections
+        // that can be activated after the optimal trade.
+        return ones + maxMise;
+    }
 };
